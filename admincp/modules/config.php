57a3ed65ba4c87caaf66c076d061ba0df838703c
Tu <tu.victory95@gmail.com>
2017-04-09 00:31:33 +0700

    Signed-off-by: Tu <tu.victory95@gmail.com>

--- admincp/modules/config.php
+++ admincp/modules/config.php
@@ -1,8 +1,8 @@
 <?php
-	$tenmaychu="localhost";
-	$tentaikhoan="root";
-	$pass="";
-	$csdl="banhang";
+	$tenmaychu="sql9.freemysqlhosting.net";
+	$tentaikhoan="sql9168124";
+	$pass="hcK1uS5kU4";
+	$csdl="sql9168124";
 	$con=mysql_connect($tenmaychu,$tentaikhoan,$pass,$csdl)or die('cant connect');
 	mysql_select_db($csdl,$con);
 	mysql_query("set names 'utf8'");
